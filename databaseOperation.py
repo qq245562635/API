10dd6dd487c6221efb52780d5f70c65c5fb3c4b2
Conansoloman <zhytford@gmail.com>
2015-08-22 16:55:45 -0400

    update

--- databaseOperation.py
+++ databaseOperation.py
@@ -6,11 +6,11 @@ import MySQLdb as mdb
 import datetime as dt
 
 
-symbol = 'AMZN'
+symbol = ['AMZN','AAPL','GOOG']
 beginDate = dt.date(2015,6,1)
 endDate = dt.date(2015,6,30)
 
-def fetchDataIntoDB(symbol = None, beginDate=None, endDate = None):
+def loadDataIntoDB(symbol = None, beginDate=None, endDate = None):
     hostName = 'localhost'
     userName = 'sec_user'
     passWord = 'password'
@@ -21,7 +21,7 @@ def fetchDataIntoDB(symbol = None, beginDate=None, endDate = None):
     yf_data = urllib2.urlopen(yahoo_url).readlines()[1:]
     for data in yf_data:
         cdata = data.split(',')
-        insertCommond = """INSERT INTO dailyStockMarket(Symbol, priceDate, openPrice, highPrice, lowPrice, closePrice, adjClosePrice, Volume) VALUES ('%s', '%s', '%s', '%s', '%s', '%s','%s','%s')"""%("AAPL",cdata[0],cdata[1],float(cdata[2]),float(cdata[3]),float(cdata[4]),float(cdata[6]),int(cdata[5]))
+        insertCommond = """INSERT INTO dailyStockMarket(Symbol, priceDate, openPrice, highPrice, lowPrice, closePrice, adjClosePrice, Volume) VALUES ('%s', '%s', '%s', '%s', '%s', '%s','%s','%s')"""%(symbol,cdata[0],cdata[1],float(cdata[2]),float(cdata[3]),float(cdata[4]),float(cdata[6]),int(cdata[5]))
         cursor.execute(insertCommond)
         db.commit()
     db.close()
@@ -35,6 +35,9 @@ def fetchDataFromDB(symbol,beginDate,endDate):
     retrieveCommond = "select * from dailyStockMarket where Symbol = '%s'and priceDate >= '%s'and priceDate <= '%s'"%(symbol,beginDate,endDate)
     cursor.execute(retrieveCommond)
     results = cursor.fetchall()
-    print results
-fetchDataFromDB('AAPL')
+    db.close()
+
+for sym in symbol:
+    loadDataIntoDB(sym,beginDate,endDate)
+
 #print yf_data
--- tradingSystem.py
+++ tradingSystem.py
@@ -1,24 +1,19 @@
 import numpy as np
 import pandas as pd
 import MySQLdb as mdb
-
-class stockMarket(object):
+'''
+def availableStockMarket():
     # Open database connection
-    db = MySQLdb.connect("localhost", "testuser", "test123", "TESTDB")
+    db = mdb.connect("localhost","sec_user", "password","securities_master")
     # Prepare a cursor object using cursor() method
     cursor = db.cursor()
-    def __init__(self):
-        pass
-    def get_price(self, stock_symbol):
-        pass
+'''
 
-
-class Portfolio(stockMarket):
-    cash = 100000
-
+class Portfolio():
+    cash = 100000.0
+    equity = 0.0
     def __init__(self):
         self.stocks = {}
-
     def buyStock(self, stock_symbol, stock_amount):
         price = stockMarket.get_price(stock_symbol)
         if price * stock_amount < self.cash:
